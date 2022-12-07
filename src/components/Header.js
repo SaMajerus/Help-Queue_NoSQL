0dc8474b9185321c4f3075bb3d15ae5e6fea20fb
Samuel Majerus <ladolego@gmail.com>
2022-12-07 10:45:25 -0800

    Utilizes the React Router library to create a 'separate' Sign-In page which uses 'client-side routing'. [End of code-along work for Lsn 20 (see link).] [https://www.learnhowtoprogram.com/react/react-with-nosql/react-router]

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 import React from "react";
-import ticketsImage from "./../img/tickets.png";
+//import ticketsImage from "./../img/tickets.png";
+import { Link } from "react-router-dom";
 
 function Header(){
   return (
     <React.Fragment>
-      <h1>Help Queue</h1>
-      <img src={ticketsImage} alt="An image of tickets" />
+      <h1> Help Queue</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/sign-in">Sign In</Link>
+        </li>
+      </ul>
+      {/* <img src={ticketsImage} alt="An image of tickets" /> */}
     </React.Fragment>
   );
 }
