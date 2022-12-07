8a9e6437e63d8983bb1b51a51d59d78337542e1d
Samuel Majerus <ladolego@gmail.com>
2022-12-07 11:16:41 -0800

    [First-half of code-along work for Lsn 21]

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,38 @@
 import React from "react";
+import { auth } from './../firebase.js';
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function SignIn(){  
+  function doSignUp(event){
+    event.preventDefault(); 
+    const email = event.target.email.value; 
+    const password = event.target.password.value; 
+
+    //It's with this function call that "Firebase authentication comes into play" (Lsn 21, 'Signing Up'). 
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // User successfully signed up
+      })
+      .catch((error) => {
+        // There was an error with Sign-Up
+      });
+  }
+
   return (
-    <h1>Sign In</h1>
+    <React.Fragment>
+      <h1>Sign up</h1>
+      <form onSubmit={doSignUp}>
+        <input
+          type='text'
+          name='email'
+          placeholder='email' />
+        <input
+          type='password'
+          name='password'
+          placeholder='Password' />
+        <button type='submit'>Sign up</button>
+      </form>
+    </React.Fragment>
   );
 }
 
--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -4,7 +4,7 @@ import TicketList from './TicketList';
 import EditTicketForm from './EditTicketForm';
 import TicketDetail from './TicketDetail';
 import { collection, addDoc, doc, updateDoc, onSnapshot, deleteDoc } from "firebase/firestore";
-import db from './../firebase.js';
+import { db } from './../firebase.js';
 
 function TicketControl() {
 
