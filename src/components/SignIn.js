0c5e8572939e5a2e522db9271992bf2d3ba74d2d
Samuel Majerus <ladolego@gmail.com>
2022-12-07 11:30:05 -0800

    Adds Sign-Up form functionality.   [Part 2 of code-along work for Lsn 21.]

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { auth } from './../firebase.js';
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";   //New import!
 
-function SignIn(){  
-  function doSignUp(event){
+function SignIn(){ 
+  const [signUpSuccess, setSignUpSuccess] = useState(null);  //New code! 
+
+  function doSignUp(event){ 
     event.preventDefault(); 
     const email = event.target.email.value; 
     const password = event.target.password.value; 
@@ -12,15 +15,19 @@ function SignIn(){
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // User successfully signed up
+        setSignUpSuccess(`You've successfully signed up, ${userCredential.user.email}!`);  //New code!
       })
       .catch((error) => {
         // There was an error with Sign-Up
+        setSignUpSuccess(`There was an error signing up: ${error.message}!`);  //New code!
       });
   }
 
   return (
     <React.Fragment>
       <h1>Sign up</h1>
+      {/* New code below! */}
+      {signUpSuccess}
       <form onSubmit={doSignUp}>
         <input
           type='text'
